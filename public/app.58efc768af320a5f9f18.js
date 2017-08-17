5dc1922500f12dbe60bcf4ee4714003d40138a8d
James Friedman <jamesmfriedman@gmail.com>
2017-08-17 11:37:59 -0400

    running build

--- public/app.58efc768af320a5f9f18.js
+++ public/app.58efc768af320a5f9f18.js
@@ -57,7 +57,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "./";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 323);
@@ -44874,7 +44874,7 @@ var App = function (_React$Component) {
 						__WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
 							__WEBPACK_IMPORTED_MODULE_2_rmdc__["ToolbarTitle"],
 							null,
-							'RMDC'
+							'ReactMDC'
 						)
 					)
 				),
