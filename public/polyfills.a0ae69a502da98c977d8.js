b170a6ee74b1feeb94ef141495bbed382c947543
James Friedman <jamesmfriedman@gmail.com>
2017-08-31 22:15:41 -0400

    running build

---
+++ public/polyfills.a0ae69a502da98c977d8.js
@@ -0,0 +1,26 @@
+webpackJsonp([2],{
+
+/***/ 352:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(353);
+
+
+/***/ }),
+
+/***/ 353:
+/***/ (function(module, exports) {
+
+;
+
+var _temp = function () {
+  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
+    return;
+  }
+}();
+
+;
+
+/***/ })
+
+},[352]);
