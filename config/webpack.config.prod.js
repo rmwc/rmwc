b0fb9d211fdc6c6834440053df524e00c4842542
James Friedman <jamesmfriedman@gmail.com>
2017-09-04 11:39:29 -0400

    More work on storybook

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -150,7 +150,8 @@ module.exports = {
             test: /\.(js|jsx)$/,
             include: [
               paths.appLib,
-              paths.appSrc
+              paths.appSrc,
+              path.resolve(paths.appSrc, "../node_modules/@material")
             ],
             loader: require.resolve('babel-loader'),
             options: {
@@ -329,6 +330,7 @@ module.exports = {
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
+    module: 'empty',
     dgram: 'empty',
     fs: 'empty',
     net: 'empty',
