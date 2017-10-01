22577a9e3d906fc3cc0591e382149648b970f959
James Friedman <jamesmfriedman@gmail.com>
2017-09-30 22:45:41 -0400

    testing ci

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,3 @@
-
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
@@ -21,6 +19,10 @@ const argv = process.argv.slice(2);
 // Watch unless on CI or in coverage mode
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
 	argv.push('--watch');
+}
+
+if (process.env.CI) {
+	argv.push('--no-cache');
 }
 
 console.log(argv);
