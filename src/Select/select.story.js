0611aeab50ecf84556b025a3fa1a3fd8d93f07dc
James Friedman <jfriedman@bigsquid.com>
2017-12-06 17:11:55 -0500

    feat(Select): allow children #43

--- src/Select/select.story.js
+++ src/Select/select.story.js
@@ -4,28 +4,45 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { text, object, array, number } from '@storybook/addon-knobs';
 import { Select } from './';
+import { ListItem } from '../List';
 
 storiesOf('Selects', module)
-	.add('Select with object', () => (
-		<Select
-			value={text('value', '')}
-			placeholder={text('placeholder', 'Select a Food')}
-			options={object('options', { 1: 'Cookies', 2: 'Pizza', 3: 'Icecream' })}
-			onChange={evt => action('onChange: ' + evt.target.value)()}
-		/>
-	))
-	.add('Select with array', () => (
-		<Select
-			value={text('value', '')}
-			placeholder={text('placeholder', 'Select a Food')}
-			options={array('options', ['Cookies', 'Pizza', 'Icecream'])}
-			onChange={evt => action('onChange: ' + evt.target.value)()}
-		/>
-	))
-	.add('Select with initial value', () => (
-		<Select
-			value={text('value', 'Cookies')}
-			options={array('options', ['Cookies', 'Pizza', 'Icecream'])}
-			onChange={evt => action('onChange: ' + evt.target.value)()}
-		/>
-	));
+  .add('Select with object', () => (
+    <Select
+      value={text('value', '')}
+      placeholder={text('placeholder', 'Select a Food')}
+      options={object('options', { 1: 'Cookies', 2: 'Pizza', 3: 'Icecream' })}
+      onChange={evt => action('onChange: ' + evt.target.value)()}
+    />
+  ))
+  .add('Select with array', () => (
+    <Select
+      value={text('value', '')}
+      placeholder={text('placeholder', 'Select a Food')}
+      options={array('options', ['Cookies', 'Pizza', 'Icecream'])}
+      onChange={evt => action('onChange: ' + evt.target.value)()}
+    />
+  ))
+  .add('Select with initial value', () => (
+    <Select
+      value={text('value', 'Cookies')}
+      options={array('options', ['Cookies', 'Pizza', 'Icecream'])}
+      onChange={evt => action('onChange: ' + evt.target.value)()}
+    />
+  ))
+  .add('Select with children', () => (
+    <Select
+      value={text('value', 'Cookies')}
+      onChange={evt => action('onChange: ' + evt.target.value)()}
+    >
+      <ListItem role="option" id="Cookies" tabIndex="0">
+        Cookies
+      </ListItem>
+      <ListItem role="option" id="Pizza" tabIndex="0">
+        Pizza
+      </ListItem>
+      <ListItem role="option" id="Icecream" tabIndex="0">
+        Icecream
+      </ListItem>
+    </Select>
+  ));
