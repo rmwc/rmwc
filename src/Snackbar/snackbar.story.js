e4603043e302df46073ef6ffcfb1b92ed78f7ac0
James Friedman <jfriedman@bigsquid.com>
2017-12-06 14:37:47 -0500

    feat(Snackbar): fixes #42 hide extra elements
    Styles the snackbar button to be hidden when no action is set and allows for extra children.

--- src/Snackbar/snackbar.story.js
+++ src/Snackbar/snackbar.story.js
@@ -7,38 +7,38 @@ import { Snackbar } from './';
 import { storyWithState } from '../Base/story-with-state';
 
 const SnackbarStory = storyWithState(
-	state => ({
-		show: boolean('show', state.show !== undefined ? state.show : true),
-		alignStart: boolean('alignStart', state.alignStart || false),
-		message: text('message', state.message || 'This is a new message'),
-		actionText: text('actionText', state.actionText || 'Action'),
-		timeout: number('timeout', state.timeout || 2750),
-		multiline: boolean('multiline', state.multiline || false),
-		actionOnBottom: boolean('actionOnBottom', state.actionOnBottom || false),
-		dismissesOnAction: boolean(
-			'dismissesOnAction',
-			state.dismissesOnAction !== undefined ? state.dismissesOnAction : true
-		)
-	}),
-	function() {
-		return (
-			<Snackbar
-				show={this.state.show}
-				message={this.state.message}
-				alignStart={this.state.alignStart}
-				actionOnBottom={this.state.actionOnBottom}
-				dismissesOnAction={this.state.dismissesOnAction}
-				multiline={this.state.multiline}
-				actionText={this.state.actionText}
-				timeout={this.state.timeout}
-				onClose={() => {
-					this.setState({ show: false });
-					action('onClose')();
-				}}
-				actionHandler={action('actionHandler')}
-			/>
-		);
-	}
+  state => ({
+    show: boolean('show', state.show !== undefined ? state.show : true),
+    alignStart: boolean('alignStart', state.alignStart || false),
+    message: text('message', state.message || 'This is a new message'),
+    actionText: text('actionText', state.actionText || 'Action'),
+    timeout: number('timeout', state.timeout || 2750),
+    multiline: boolean('multiline', state.multiline || false),
+    actionOnBottom: boolean('actionOnBottom', state.actionOnBottom || false),
+    dismissesOnAction: boolean(
+      'dismissesOnAction',
+      state.dismissesOnAction !== undefined ? state.dismissesOnAction : true
+    )
+  }),
+  function() {
+    return (
+      <Snackbar
+        show={this.state.show}
+        message={this.state.message}
+        alignStart={this.state.alignStart}
+        actionOnBottom={this.state.actionOnBottom}
+        dismissesOnAction={this.state.dismissesOnAction}
+        multiline={this.state.multiline}
+        actionText={this.state.actionText}
+        timeout={this.state.timeout}
+        onClose={() => {
+          this.setState({ show: false });
+          action('onClose')();
+        }}
+        actionHandler={action('actionHandler')}
+      />
+    );
+  }
 );
 
 storiesOf('Snackbar', module).add('Snackbar', () => <SnackbarStory />);
