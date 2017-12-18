b82ea87a4579c1583486c0e4c10d2038b7639bc8
James Friedman <jfriedman@bigsquid.com>
2017-12-18 10:52:39 -0500

    feat(ListItem): Added select and activated props

--- src/List/list.spec.js
+++ src/List/list.spec.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import {
-	List,
-	ListItem,
-	ListItemText,
-	ListItemStartDetail,
-	ListItemEndDetail
+  List,
+  ListItem,
+  ListItemText,
+  ListItemStartDetail,
+  ListItemEndDetail
 } from './';
 
 describe('List', () => {
-	it('renders', () => {
-		mount(
-			<List>
-				<ListItem ripple>
-					<ListItemStartDetail>
-						<i />
-					</ListItemStartDetail>
-					<ListItemText>Cookies</ListItemText>
-					<ListItemEndDetail>
-						<i />
-					</ListItemEndDetail>
-				</ListItem>
-			</List>
-		);
-	});
+  it('renders', () => {
+    mount(
+      <List>
+        <ListItem ripple>
+          <ListItemStartDetail>
+            <i />
+          </ListItemStartDetail>
+          <ListItemText>Cookies</ListItemText>
+          <ListItemEndDetail>
+            <i />
+          </ListItemEndDetail>
+        </ListItem>
+      </List>
+    );
+  });
 
-	it('can have custom classnames', () => {
-		[List, ListItem, ListItemText].forEach(Component => {
-			const el = mount(<Component className={'my-custom-classname'} />);
-			expect(!!~el.html().search('my-custom-classname')).toEqual(true);
-		});
-	});
+  it('can have custom classnames', () => {
+    [List, ListItem, ListItemText].forEach(Component => {
+      const el = mount(<Component className={'my-custom-classname'} />);
+      expect(!!~el.html().search('my-custom-classname')).toEqual(true);
+    });
+  });
+
+  it('can be activated', () => {
+    const el = mount(<ListItem activated />);
+    expect(!!~el.html().search('mdc-list-item--activated')).toEqual(true);
+  });
+
+  it('can be selected', () => {
+    const el = mount(<ListItem selected />);
+    expect(!!~el.html().search('mdc-list-item--selected')).toEqual(true);
+  });
 });
