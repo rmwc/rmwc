a6feed8bad8c64b73e428480cf5330d04db36e87
James Friedman <jfriedman@bigsquid.com>
2017-12-19 14:43:44 -0500

    fixing tests

--- src/Fab/fab.spec.js
+++ src/Fab/fab.spec.js
@@ -3,28 +3,28 @@ import { mount } from 'enzyme';
 import { Fab } from './';
 
 describe('Fab', () => {
-	it('renders', () => {
-		const btn = mount(<Fab />);
-		expect(!!~btn.html().search('mdc-fab')).toEqual(true);
-	});
+  it('renders', () => {
+    const btn = mount(<Fab />);
+    expect(!!~btn.html().search('mdc-fab')).toEqual(true);
+  });
 
-	it('can be mini', () => {
-		const btn = mount(<Fab mini />);
-		expect(!!~btn.html().search('mdc-fab--mini')).toEqual(true);
-	});
+  it('can be mini', () => {
+    const btn = mount(<Fab mini />);
+    expect(!!~btn.html().search('mdc-fab--mini')).toEqual(true);
+  });
 
-	it('can ripple', () => {
-		const btn = mount(<Fab ripple />);
-		expect(!!~btn.html().search('mdc-ripple-surface')).toEqual(true);
-	});
+  it('can ripple', () => {
+    const btn = mount(<Fab ripple />);
+    expect(!!~btn.html().search('mdc-fab')).toEqual(true);
+  });
 
-	it('cannot ripple', () => {
-		const btn = mount(<Fab ripple={false} />);
-		expect(!!~btn.html().search('mdc-ripple-surface')).toEqual(false);
-	});
+  it('cannot ripple', () => {
+    const btn = mount(<Fab ripple={false} />);
+    expect(!!~btn.html().search('mdc-ripple-surface')).toEqual(false);
+  });
 
-	it('can have custom classnames', () => {
-		const el = mount(<Fab className={'my-custom-classname'} />);
-		expect(!!~el.html().search('my-custom-classname')).toEqual(true);
-	});
+  it('can have custom classnames', () => {
+    const el = mount(<Fab className={'my-custom-classname'} />);
+    expect(!!~el.html().search('my-custom-classname')).toEqual(true);
+  });
 });
