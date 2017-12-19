3dc907506f5a0cefc54f02cb8fa301ab331365fa
James Friedman <jfriedman@bigsquid.com>
2017-12-19 15:30:33 -0500

    Adding select documentation

--- docs/content.js
+++ docs/content.js
@@ -375,16 +375,130 @@ export default [
     url:
       'https://material.io/components/web/catalog/input-controls/select-menus/',
     example: `
+			{/*  
+				Select Menus
+				RMWC implements a data driven method for passing options to a select.
+				There are multiple ways you can pass data in, use the one that best fulfills your requirements.
+			*/}
+			
+			{/* a formatted array of options */}
 			<Select
-				placeholder="Select a food"
-				options={{1: 'Cookies', 2: 'Pizza', 3: 'Icecream'}}
-			/>
+				value={this.state.value}
+				onChange={evt => this.setState({value: evt.target.value})}
+				label="Array"
+				options={[
+					{
+						label: 'Cookies',
+						value: '1'
+					},
+					{
+						label: 'Pizza',
+						value: '2',
+						
+						/** Any additional items will be past to the
+						 child list item as props */
 
-			{/* as an array of options */}
+						'aria-disabled': true,
+						'tab-index': -1
+					},
+					{
+						label: 'Icecream',
+						value: '3'
+					}
+				]}
+			/>
+			<br/>
+
+			{/*  A simple value => label map */}
 			<Select
-				value={'Pizza'}
+				label="Object map"
+				options={{'1': 'Cookies', '2': 'Pizza', '3': 'Icecream'}}
+			/>
+			<br/>
+
+			{/* a simple array of options, value will be the same as label */}
+			<Select
+				label="Simple Array"
+				placeholder="-- Select One --"
 				options={['Cookies', 'Pizza', 'Icecream']}
 			/>
+			<br/>
+
+			{/* manually build the child list yourself */}
+			<Select
+				label="Manually Built"
+				value={'Pizza'}
+			>
+				<ListItem role="option" id="Cookies" tabIndex="0">
+					Cookies
+				</ListItem>
+				<ListItem role="option" id="Pizza" tabIndex="0">
+					Pizza
+				</ListItem>
+				<ListItem role="option" id="Icecream" tabIndex="0">
+					Icecream
+				</ListItem>
+			</Select>
+			<br/>
+
+			{/* 
+				cssOnly Selects
+				MDC doesnt have a label prop for css only selects, but RMWC has a placeholder prop you can use.
+			*/}
+			<Select
+				cssOnly
+				placeholder="-- cssOnly --"
+				options={['Cookies', 'Pizza', 'Icecream']}
+			/>
+			<br/>
+
+			{/* 
+				cssOnly Selects with option groups
+				MDC only supports option groups on css only selects.
+			*/}
+			<Select
+				cssOnly
+				placeholder="-- cssOnly w/ optGroups --"
+				options={[
+					{
+						label: 'Foods',
+						/* Options can be any value Select input: simply arrays, value => label objects, or formatted arrays. */
+						options: ['Cookies', 'Pizza', 'Icecream']
+					},
+					{ 
+						label: 'Animals',
+						options: ['Dogs', 'Cats', 'Birds']
+					}
+				]}
+			/>
+			<br/>
+
+			{/* 
+				cssOnly Multi-select
+				MDC only supports multiple on css only selects.
+				Does not support labels or placeholders
+			*/}
+			<Select
+				cssOnly
+				multiple
+        size="8"
+				value={this.state.multi || []}
+				onChange={evt =>  this.setState({
+					multi: [...evt.target.selectedOptions].map(o => o.value)
+				})}
+				options={[
+					{
+						label: 'Foods',
+						/* Options can be any value Select input: simply arrays, value => label objects, or formatted arrays. */
+						options: ['Cookies', 'Pizza', 'Icecream']
+					},
+					{ 
+						label: 'Animals',
+						options: ['Dogs', 'Cats', 'Birds']
+					}
+				]}
+			/>
+
 		`
   },
   {
