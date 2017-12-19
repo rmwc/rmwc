93431faf4f8e40edcbde6365920b674cbd646393
James Friedman <jfriedman@bigsquid.com>
2017-12-19 14:40:51 -0500

    Fixing button tests

--- src/Button/button.spec.js
+++ src/Button/button.spec.js
@@ -3,48 +3,38 @@ import { mount } from 'enzyme';
 import Button from './';
 
 describe('Button', () => {
-	it('renders', () => {
-		const btn = mount(<Button />);
-		expect(!!~btn.html().search('mdc-button')).toEqual(true);
-	});
+  it('renders', () => {
+    const btn = mount(<Button />);
+    expect(!!~btn.html().search('mdc-button')).toEqual(true);
+  });
 
-	it('can be raised', () => {
-		const btn = mount(<Button raised />);
-		expect(!!~btn.html().search('mdc-button--raised')).toEqual(true);
-	});
+  it('can be raised', () => {
+    const btn = mount(<Button raised />);
+    expect(!!~btn.html().search('mdc-button--raised')).toEqual(true);
+  });
 
-	it('can be unelevated', () => {
-		const btn = mount(<Button unelevated />);
-		expect(!!~btn.html().search('mdc-button--unelevated')).toEqual(true);
-	});
+  it('can be unelevated', () => {
+    const btn = mount(<Button unelevated />);
+    expect(!!~btn.html().search('mdc-button--unelevated')).toEqual(true);
+  });
 
-	it('can be stroked', () => {
-		const btn = mount(<Button stroked />);
-		expect(!!~btn.html().search('mdc-button--stroked')).toEqual(true);
-	});
+  it('can be stroked', () => {
+    const btn = mount(<Button stroked />);
+    expect(!!~btn.html().search('mdc-button--stroked')).toEqual(true);
+  });
 
-	it('can be dense', () => {
-		const btn = mount(<Button dense />);
-		expect(!!~btn.html().search('mdc-button--dense')).toEqual(true);
-	});
+  it('can be dense', () => {
+    const btn = mount(<Button dense />);
+    expect(!!~btn.html().search('mdc-button--dense')).toEqual(true);
+  });
 
-	it('can be compact', () => {
-		const btn = mount(<Button compact />);
-		expect(!!~btn.html().search('mdc-button--compact')).toEqual(true);
-	});
+  it('can be compact', () => {
+    const btn = mount(<Button compact />);
+    expect(!!~btn.html().search('mdc-button--compact')).toEqual(true);
+  });
 
-	it('can ripple', () => {
-		const btn = mount(<Button ripple />);
-		expect(!!~btn.html().search('mdc-ripple-surface')).toEqual(true);
-	});
-
-	it('cannot ripple', () => {
-		const btn = mount(<Button ripple={false} />);
-		expect(btn.hasClass('mdc-ripple-surface')).toEqual(false);
-	});
-
-	it('can have custom classnames', () => {
-		const el = mount(<Button className={'my-custom-classname'} />);
-		expect(!!~el.html().search('my-custom-classname')).toEqual(true);
-	});
+  it('can have custom classnames', () => {
+    const el = mount(<Button className={'my-custom-classname'} />);
+    expect(!!~el.html().search('my-custom-classname')).toEqual(true);
+  });
 });
