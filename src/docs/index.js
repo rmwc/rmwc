b51e777cf0ab783707b6203b589d4185712c564f
James Friedman <jfriedman@bigsquid.com>
2018-01-19 16:29:51 -0500

    feat(Icon): Added custom icon rendering hooks

--- src/docs/index.js
+++ src/docs/index.js
@@ -10,14 +10,17 @@ import { AppContainer } from 'react-hot-loader';
 import App from './App';
 import { unregister } from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { RMWCProvider } from 'rmwc/Provider';
 
 const renderApp = Component => {
   ReactDOM.render(
-    <AppContainer>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Component location={window.location.href} />
-      </Router>
-    </AppContainer>,
+    <RMWCProvider>
+      <AppContainer>
+        <Router basename={process.env.PUBLIC_URL}>
+          <Component location={window.location.href} />
+        </Router>
+      </AppContainer>
+    </RMWCProvider>,
     document.getElementById('root')
   );
 };
