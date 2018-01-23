1627c6fe4e7fddc81787b54cc48b00452e85a4b4
James Friedman <me@jamesmfriedman.com>
2018-01-23 12:22:17 -0500

    docs(DarkMode): documenting dark mode and adding a global toggle to docs.

--- src/Switch/switch.spec.js
+++ src/Switch/switch.spec.js
@@ -3,28 +3,28 @@ import { mount } from 'enzyme';
 import Switch from './';
 
 describe('Switch', () => {
-	it('renders', () => {
-		const switchEl = mount(<Switch />);
-		expect(!!~switchEl.html().search('mdc-switch')).toEqual(true);
-	});
+  it('renders', () => {
+    const switchEl = mount(<Switch />);
+    expect(!!~switchEl.html().search('mdc-switch')).toEqual(true);
+  });
 
-	it('can be checked', () => {
-		const switchEl = mount(<Switch checked={true} onChange={() => {}} />);
-		expect(switchEl.find('input').getDOMNode().checked).toEqual(true);
-	});
+  it('can be checked', () => {
+    const switchEl = mount(<Switch checked onChange={() => {}} />);
+    expect(switchEl.find('input').getDOMNode().checked).toEqual(true);
+  });
 
-	it('can be disabled', () => {
-		const switchEl = mount(<Switch disabled />);
-		expect(switchEl.find('input').getDOMNode().disabled).toEqual(true);
-	});
+  it('can be disabled', () => {
+    const switchEl = mount(<Switch disabled />);
+    expect(switchEl.find('input').getDOMNode().disabled).toEqual(true);
+  });
 
-	it('can have a label', () => {
-		const switchEl = mount(<Switch label="hello world" />);
-		expect(switchEl.text()).toEqual('hello world');
-	});
+  it('can have a label', () => {
+    const switchEl = mount(<Switch label="hello world" />);
+    expect(switchEl.text()).toEqual('hello world');
+  });
 
-	it('can have custom classnames on input', () => {
-		const el = mount(<Switch className={'my-custom-classname'} />);
-		expect(!!~el.html().search('my-custom-classname')).toEqual(true);
-	});
+  it('can have custom classnames on input', () => {
+    const el = mount(<Switch className={'my-custom-classname'} />);
+    expect(!!~el.html().search('my-custom-classname')).toEqual(true);
+  });
 });
