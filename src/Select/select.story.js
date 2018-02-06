f69904665f87667dc536a5ab9b0894ddb76370a4
j-o-d-o <johannes_dob@web.de>
2018-02-06 10:10:16 +0100

    breaking(select): remove cssOnly select

--- src/Select/select.story.js
+++ src/Select/select.story.js
@@ -7,31 +7,9 @@ import { Select } from './';
 import { ListItem } from '../List';
 import { storyWithState } from '../Base/story-with-state';
 
-const CSSSelectStory = storyWithState(
-  state => ({
-    label: text('label', state.label || 'Foods'),
-    value: text('value', state.value || 'Cookies'),
-    options: array('options', state.options || ['Cookies', 'Pizza', 'Icecream'])
-  }),
-  function() {
-    return (
-      <Select
-        cssOnly
-        label={this.state.label}
-        value={this.state.value}
-        options={this.state.options}
-        onChange={evt => {
-          this.setState({ value: evt.target.value });
-          action('onChange: ' + evt.target.value)();
-        }}
-      />
-    );
-  }
-);
 
 const MutatingSelect = storyWithState(
   state => ({
-    cssOnly: boolean('cssOnly', state.cssOnly || false),
     value: text('value', state.value || 'Cookies'),
     label: text('label', state.label || 'Label'),
     options: array('options', state.options || ['Cookies', 'Pizza', 'Icecream'])
@@ -40,36 +18,6 @@ const MutatingSelect = storyWithState(
     return (
       <Select
         {...this.props}
-        cssOnly={this.state.cssOnly}
-        label={this.state.label}
-        value={this.state.value}
-        options={this.state.options}
-        onChange={evt => {
-          this.setState({ value: evt.target.value });
-          action('onChange: ' + evt.target.value)();
-        }}
-      />
-    );
-  }
-);
-
-const CSSSelectWithOptgroupsStory = storyWithState(
-  state => ({
-    label: text('label', state.label || 'Foods'),
-    value: text('value', state.value || 'Cookies'),
-    options: array(
-      'options',
-      state.options || [
-        { label: 'Foods', options: ['Cookies', 'Pizza', 'Icecream'] },
-        { label: 'Animals', options: ['Dogs', 'Cats', 'Birds'] }
-      ]
-    )
-  }),
-  function() {
-    return (
-      <Select
-        {...this.props}
-        cssOnly
         label={this.state.label}
         value={this.state.value}
         options={this.state.options}
@@ -125,6 +73,4 @@ storiesOf('Inputs and Controls', module)
       </ListItem>
     </Select>
   ))
-  .add('CSS Select', () => <CSSSelectStory />)
-  .add('CSS Select w/ optgroups', () => <CSSSelectWithOptgroupsStory />)
   .add('Mutating Select', () => <MutatingSelect />);
