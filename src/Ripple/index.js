9e61e547dc7ac5ac063c2fc5584a68bef2ed7685
James Friedman <jfriedman@bigsquid.com>
2018-03-15 15:49:24 -0400

    chore(Test): Improving tests for IconToggle and Slider

--- src/Ripple/index.js
+++ src/Ripple/index.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import { MDCRipple } from '@material/ripple/dist/mdc.ripple';
-import { simpleTag } from '../Base';
+
 import {
   withFoundation,
   addClass,
@@ -59,7 +59,13 @@ export class Ripple extends withFoundation({
     const { root_ } = this.foundationRefs;
 
     const child = React.Children.only(children);
-    const refProp = { [child.type.isSimpleTag ? 'elementRef' : 'ref']: root_ };
+
+    // a little tricky... We only want to pass a ref if we are dealing with a dom element, aka div, p, aside
+    // Otherwise we have a class, and we want to pass elementRef down the chain.
+    const refProp = {
+      [typeof child.type !== 'string' ? 'elementRef' : 'ref']: root_
+    };
+
     const unboundedProp = unbounded ?
       { 'data-mdc-ripple-is-unbounded': true } :
       {};
@@ -69,11 +75,16 @@ export class Ripple extends withFoundation({
       ...rest,
       ...refProp,
       ...unboundedProp,
-      className: classNames(className, [...this.state.classes], {
-        'mdc-ripple-surface': !child.type.isSimpleTag,
-        'mdc-ripple-surface--primary': primary,
-        'mdc-ripple-surface--accent': accent
-      })
+      className: classNames(
+        className,
+        child.props.className,
+        [...this.state.classes],
+        {
+          'mdc-ripple-surface': !child.type.isSimpleTag,
+          'mdc-ripple-surface--primary': primary,
+          'mdc-ripple-surface--accent': accent
+        }
+      )
     });
   }
 }
