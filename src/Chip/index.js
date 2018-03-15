11771104f28a2bec2a20abcb1122013fa2b7f186
James Friedman <jfriedman@bigsquid.com>
2018-03-15 16:55:54 -0400

    feat(Chip): Implemented with Foundation adapter, upgraded to current spec.

--- src/Chip/index.js
+++ src/Chip/index.js
@@ -1,14 +1,58 @@
 // @flow
 
 import * as React from 'react';
+import classNames from 'classnames';
 import { Icon } from '../Icon';
-import { simpleTag } from '../Base';
+import { simpleTag, withRipple } from '../Base';
+import { withFoundation, addClass, removeClass } from '../Base/MDCFoundation';
+
+import { MDCChip } from '@material/chips/dist/mdc.chips';
 
 /** A chip component. */
-export const Chip = simpleTag({
-  displayName: 'Chip',
-  classNames: 'mdc-chip'
-});
+export const ChipRoot = withRipple()(
+  simpleTag({
+    displayName: 'ChipRoot',
+    classNames: props => [
+      'mdc-chip',
+      {
+        'mdc-chip--selected': props.selected
+      }
+    ],
+    consumeProps: ['selected']
+  })
+);
+
+type ChipPropsT = {
+  /** A custom event you can use from MCW. You can also just use onClick instead. */
+  onInteraction?: (evt: Event) => mixed,
+  /** A custom event for the trailing icon that you can use from MCW. You can also just use onClick instead. */
+  onTrailingIconInteraction?: (evt: Event) => mixed,
+  /** makes the Chip appear selected. */
+  selected?: boolean
+};
+
+export class Chip extends withFoundation({
+  constructor: MDCChip,
+  adapter: {
+    addClass: addClass(),
+    removeClass: removeClass()
+  }
+})<ChipPropsT> {
+  static displayName = 'Chip';
+
+  render() {
+    const { onInteraction, onTrailingIconInteraction, ...rest } = this.props;
+    const { root_ } = this.foundationRefs;
+    return (
+      <ChipRoot
+        tabIndex={0}
+        {...rest}
+        elementRef={root_}
+        className={classNames(this.props.className, [...this.state.classes])}
+      />
+    );
+  }
+}
 
 /** Text for a chip. */
 export const ChipText = simpleTag({
@@ -16,17 +60,11 @@ export const ChipText = simpleTag({
   classNames: 'mdc-chip__text'
 });
 
-type ChipIconPropsT = {
-  /** Make it a leading icon */
-  leading: boolean,
-  /** Make it a trailing icon */
-  trailing: boolean
-};
-
 /** Icons inside of a chip. This is an instance of the Icon component. To make the icons interactive, add props tabIndex="0" and role="button". */
-export class ChipIcon extends simpleTag({
-  displayName: 'ChipIcon',
+export const ChipIconRoot = simpleTag({
+  displayName: 'ChipIconRoot',
   tag: Icon,
+  defaultProps: {},
   classNames: props => {
     return [
       'mdc-chip__icon',
@@ -37,14 +75,37 @@ export class ChipIcon extends simpleTag({
     ];
   },
   consumeProps: ['trailing', 'leading']
-})<ChipIconPropsT> {
-  render() {
-    return super.render();
-  }
-}
+});
+
+type ChipIconPropsT = {
+  /** Make it a leading icon */
+  leading: boolean,
+  /** Make it a trailing icon */
+  trailing: boolean
+};
+export const ChipIcon = (props: ChipIconPropsT) => {
+  const hasInteractionHandler = Object.keys(props).some(p =>
+    p.startsWith('on')
+  );
+  const trailingProps =
+    props.trailing || hasInteractionHandler ?
+      { role: 'button', tabIndex: 0 } :
+      {};
+
+  return <ChipIconRoot {...trailingProps} {...props} />;
+};
+
+ChipIcon.displayName = 'ChipIcon';
 
 /** A container for multiple Chip components. */
 export const ChipSet = simpleTag({
   displayName: 'ChipSet',
-  classNames: 'mdc-chip-set'
+  classNames: props => [
+    'mdc-chip-set',
+    {
+      'mdc-chip-set--choice': props.choice,
+      'mdc-chip-set--filter': props.filter
+    }
+  ],
+  consumeProps: ['filter', 'choice']
 });
