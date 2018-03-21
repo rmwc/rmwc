93c2487e2b8a0e8784b0228319732b638f72ca93
James Friedman <me@jamesmfriedman.com>
2018-03-21 09:37:07 -0400

    fix(SSR): Corrects server side rendering for foundation based components

---
+++ src/Slider/slider-ssr.spec.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment node
+ */
+import React from 'react';
+import { renderToString as mount } from 'react-dom/server';
+import { Slider } from './';
+
+describe('Slider SSR', () => {
+  it('renders uncontrolled', () => {
+    mount(<Slider />);
+  });
+
+  it('renders with all props', () => {
+    mount(
+      <Slider
+        value={50}
+        discrete
+        displayMarkers
+        min={0}
+        max={100}
+        step={10}
+        disabled
+        onChange={() => {}}
+        onInput={() => {}}
+      />
+    );
+  });
+});
