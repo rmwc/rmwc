a56890e1fb8b364544887bfb1411ab8567e3af04
James Friedman <jfriedman@bigsquid.com>
2018-04-05 13:47:17 -0400

    feat(Grid): Added alignment and ordering props #179 closes #178

--- src/Grid/grid.spec.js
+++ src/Grid/grid.spec.js
@@ -3,17 +3,50 @@ import { mount } from 'enzyme';
 import { Grid, GridCell } from './';
 
 describe('Grid', () => {
-	it('renders', () => {
-		const grid = mount(<Grid />);
-		const gridCell = mount(<GridCell />);
-		expect(!!~grid.html().search('mdc-layout-grid')).toEqual(true);
-		expect(!!~gridCell.html().search('mdc-layout-grid__cell')).toEqual(true);
-	});
+  it('renders', () => {
+    const grid = mount(<Grid />);
+    const gridCell = mount(<GridCell />);
+    expect(!!~grid.html().search('mdc-layout-grid')).toEqual(true);
+    expect(!!~gridCell.html().search('mdc-layout-grid__cell')).toEqual(true);
+  });
 
-	it('can have custom classnames', () => {
-		[Grid, GridCell].forEach(Component => {
-			const el = mount(<Component className={'my-custom-classname'} />);
-			expect(!!~el.html().search('my-custom-classname')).toEqual(true);
-		});
-	});
+  it('can be fixedColumnWidth', () => {
+    mount(<Grid fixedColumnWidth />);
+  });
+
+  it('can be align', () => {
+    mount(<Grid align="left" />);
+    mount(<Grid align="right" />);
+  });
+
+  it('GridCell span', () => {
+    mount(<GridCell span="4" />);
+  });
+
+  it('GridCell phone', () => {
+    mount(<GridCell phone="4" />);
+  });
+
+  it('GridCell tablet', () => {
+    mount(<GridCell tablet="4" />);
+  });
+
+  it('GridCell desktop', () => {
+    mount(<GridCell desktop="4" />);
+  });
+
+  it('GridCell order', () => {
+    mount(<GridCell order="4" />);
+  });
+
+  it('GridCell align', () => {
+    mount(<GridCell align="top" />);
+  });
+
+  it('can have custom classnames', () => {
+    [Grid, GridCell].forEach(Component => {
+      const el = mount(<Component className={'my-custom-classname'} />);
+      expect(!!~el.html().search('my-custom-classname')).toEqual(true);
+    });
+  });
 });
