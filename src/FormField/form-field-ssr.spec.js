495fda3200f487440b71c531019ea353d7cd9a54
James Friedman <me@jamesmfriedman.com>
2018-05-02 11:56:48 -0400

    chore(FormField): Converting to foundation component #210 #194

---
+++ src/FormField/form-field-ssr.spec.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment node
+ */
+import React from 'react';
+import { renderToString as mount } from 'react-dom/server';
+import { FormField } from './';
+
+describe('FormField SSR', () => {
+  test('renders', () => {
+    mount(
+      <FormField>
+        <input type="checkbox" id="input" />
+        <label htmlFor="input">Input Label</label>
+      </FormField>
+    );
+  });
+});
