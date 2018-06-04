84a02f034f4ed12be825a0c384614986cd8ef033
James Friedman <jfriedman@bigsquid.com>
2018-06-04 11:41:35 -0400

    chore(LinearProgress): Refactor to Foundation pattern #239 #195 #141

--- src/LinearProgress/linear-progress.spec.js
+++ src/LinearProgress/linear-progress.spec.js
@@ -3,32 +3,32 @@ import { mount } from 'enzyme';
 import { LinearProgress } from './';
 
 describe('LinearProgress', () => {
-	it('renders', () => {
-		const el = mount(<LinearProgress progress={0.5} />);
-		expect(!!~el.html().search('mdc-linear-progress')).toBe(true);
-	});
+  it('renders', () => {
+    const el = mount(<LinearProgress progress={0.5} />);
+    expect(el.html().includes('mdc-linear-progress')).toBe(true);
+  });
 
-	it('can be accent', () => {
-		const el = mount(<LinearProgress accent />);
-		expect(!!~el.html().search('mdc-linear-progress--accent')).toBe(true);
-	});
+  it('can be accent', () => {
+    const el = mount(<LinearProgress accent />);
+    expect(el.html().includes('mdc-linear-progress--accent')).toBe(true);
+  });
 
-	it('can buffer', () => {
-		const el = mount(<LinearProgress buffer={0.8} />);
-	});
+  it('can buffer', () => {
+    const el = mount(<LinearProgress buffer={0.8} />);
+  });
 
-	it('can be indeterminate', () => {
-		const el = mount(<LinearProgress determinate={false} />);
-		expect(!!~el.html().search('mdc-linear-progress--determinate')).toBe(false);
-	});
+  it('can be indeterminate', () => {
+    const el = mount(<LinearProgress determinate={false} />);
+    expect(el.html().includes('mdc-linear-progress--determinate')).toBe(false);
+  });
 
-	it('can be reversed', () => {
-		const el = mount(<LinearProgress reversed />);
-		expect(!!~el.html().search('mdc-linear-progress--reversed')).toBe(true);
-	});
+  it('can be reversed', () => {
+    const el = mount(<LinearProgress reversed />);
+    expect(el.html().includes('mdc-linear-progress--reversed')).toBe(true);
+  });
 
-	it('can have custom classnames', () => {
-		const el = mount(<LinearProgress className={'my-custom-classname'} />);
-		expect(!!~el.html().search('my-custom-classname')).toEqual(true);
-	});
+  it('can have custom classnames', () => {
+    const el = mount(<LinearProgress className={'my-custom-classname'} />);
+    expect(el.html().includes('my-custom-classname')).toEqual(true);
+  });
 });
