2746a112bfa3068f3303b7265a1f720b2aa5b6e4
James Friedman <jfriedman@bigsquid.com>
2018-07-13 13:26:35 -0400

    Adding codecov

--- scripts/test.js
+++ scripts/test.js
@@ -4,11 +4,13 @@ const childProcess = require('child_process');
 fs.readdir('react-versions', function(err, files) {
   const reactVersions = files.filter(f => f.startsWith('react-'));
 
-  reactVersions.forEach(reactVersion => {
+  reactVersions.forEach((reactVersion, index, arr) => {
     const versionNum = reactVersion.split('-')[1];
+    const isLastRun = index === arr.length - 1;
     console.log('Running tests for', versionNum);
     childProcess.execSync(
-      `export REACT_TEST_VERSION=${versionNum} CI=true && react-app-rewired test --env=jsdom`,
+      //prettier-ignore
+      `export REACT_TEST_VERSION=${versionNum} CI=true && react-app-rewired test --env=jsdom ${isLastRun ? '--coverage' : ''}`,
       { stdio: [0, 1, 2] }
     );
   });
