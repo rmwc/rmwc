b8c27d40080a82c0bb2d9b908d40819bf13801e3
James Friedman <me@jamesmfriedman.com>
2019-02-08 09:37:21 -0500

    Fixing export

--- src/base/index.tsx
+++ src/base/index.tsx
@@ -1,15 +1,14 @@
 import { WithThemeProps as _WithThemeProps } from './with-theme';
 import { FoundationProps as _FoundationProps } from './foundation-component';
 import { FocusTrap as _FocusTrap } from 'focus-trap';
-import _classNames from 'classnames';
 import _createFocusTrap from 'focus-trap';
 
+export { default as classNames } from 'classnames';
 export * from './with-theme';
 export * from './utils';
 export { FoundationComponent } from './foundation-component';
 export { componentFactory } from './component';
 
-export const classNames = _classNames;
 export const createFocusTrap = _createFocusTrap;
 
 export type WithThemeProps = _WithThemeProps;
