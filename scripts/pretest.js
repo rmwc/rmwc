cc591f33c3b880b4ae6ed4f1ed13543575377697
James Friedman <me@jamesmfriedman.com>
2019-02-08 09:38:55 -0500

    Adding React 16.8

--- scripts/pretest.js
+++ scripts/pretest.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const childProcess = require('child_process');
 
-['16.3.x', '16.4.x', '16.5.x', '16.6.x', '16.7.x'].forEach(version => {
+['16.3.x', '16.4.x', '16.5.x', '16.6.x', '16.7.x', '16.8.x'].forEach(version => {
   if (!fs.existsSync(`react-versions/react-${version}`)) {
     childProcess.execSync(
       `npm i react-test-renderer@${version} react-dom@${version} react@${version} --prefix react-versions/react-${version}`,
