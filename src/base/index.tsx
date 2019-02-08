3d3337eacd725b34eb60b7a67f657ce13ed805db
James Friedman <me@jamesmfriedman.com>
2019-02-08 09:31:43 -0500

    Fixing focus trap export

--- src/base/index.tsx
+++ src/base/index.tsx
@@ -1,17 +1,17 @@
 import { WithThemeProps as _WithThemeProps } from './with-theme';
 import { FoundationProps as _FoundationProps } from './foundation-component';
 import { FocusTrap as _FocusTrap } from 'focus-trap';
-export { default as classNames } from 'classnames';
-export { default as createFocusTrap } from 'focus-trap';
+import _classNames from 'classnames';
+import _createFocusTrap from 'focus-trap';
 
 export * from './with-theme';
 export * from './utils';
 export { FoundationComponent } from './foundation-component';
 export { componentFactory } from './component';
 
+export const classNames = _classNames;
+export const createFocusTrap = _createFocusTrap;
+
 export type WithThemeProps = _WithThemeProps;
 export interface FoundationProps extends _FoundationProps {}
 export interface FocusTrap extends _FocusTrap {}
-
-
-
