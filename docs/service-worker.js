9da5d406bd060cc00adf3e57d7b2409255ac1d48
me@jamesmfriedman.com <me@jamesmfriedman.com>
2019-05-06 11:06:11 -0400

    Testing expsing old docs version

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74f7c97f0c8a6f441facd04b65c5d9dd.js"
+  "/precache-manifest.9326779b3f6e5610b9dc4aeb3950eca1.js"
 );
 
 self.addEventListener('message', (event) => {
