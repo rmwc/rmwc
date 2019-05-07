cf1592096c940848ac1a36fa81966917775a77f2
me@jamesmfriedman.com <me@jamesmfriedman.com>
2019-05-07 13:28:37 -0400

    Deploying docs

---
+++ docs/precache-manifest.c10f995760201b878ceec93b8649e8a8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbde9c1167baca4d26dff166e650f03f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aca8180b4f908c5afd1d",
+    "url": "/static/css/2.7bec0e2f.chunk.css"
+  },
+  {
+    "revision": "90705bd53f962dcc533a",
+    "url": "/static/css/5.9a18bed2.chunk.css"
+  },
+  {
+    "revision": "d109bcd91956d2de3490",
+    "url": "/static/css/main.ff7223ab.chunk.css"
+  },
+  {
+    "revision": "aca8180b4f908c5afd1d",
+    "url": "/static/js/2.0a8cc044.chunk.js"
+  },
+  {
+    "revision": "64fa6f8465475f788e46",
+    "url": "/static/js/3.a559c67b.chunk.js"
+  },
+  {
+    "revision": "16e2a22b16c78e876df5",
+    "url": "/static/js/4.1eadb8a2.chunk.js"
+  },
+  {
+    "revision": "90705bd53f962dcc533a",
+    "url": "/static/js/5.fe61c175.chunk.js"
+  },
+  {
+    "revision": "b9aa8c09c9064b0e9ffe",
+    "url": "/static/js/6.88182679.chunk.js"
+  },
+  {
+    "revision": "e0d2397b9fd0c7ac4f56",
+    "url": "/static/js/7.16fc391d.chunk.js"
+  },
+  {
+    "revision": "1bda910014ac4e563df9",
+    "url": "/static/js/8.fd24a23c.chunk.js"
+  },
+  {
+    "revision": "d109bcd91956d2de3490",
+    "url": "/static/js/main.e20e85b9.chunk.js"
+  },
+  {
+    "revision": "c27c2f1f52e73d447c40",
+    "url": "/static/js/runtime~main.c9cbb162.js"
+  },
+  {
+    "revision": "46d94d0b9b86c849953cb65962629dfd",
+    "url": "/static/media/README-INSTALLATION.46d94d0b.md"
+  },
+  {
+    "revision": "be3aee2739fe10a79e54234434071858",
+    "url": "/static/media/README-LIBRARY-INTEGRATIONS.be3aee27.md"
+  },
+  {
+    "revision": "d46870bbbf89397afa2b85c18641f97e",
+    "url": "/static/media/README-METHODOLOGY.d46870bb.md"
+  },
+  {
+    "revision": "7907df0f319b3c95af9d01ff06847136",
+    "url": "/static/media/README-STYLING-THEMING.7907df0f.md"
+  },
+  {
+    "revision": "35bcc832c2d31af090ded096b0d4250b",
+    "url": "/static/media/README-TYPES.35bcc832.md"
+  },
+  {
+    "revision": "7390ed02487e96b71d955f518487a171",
+    "url": "/static/media/README-USAGE.7390ed02.md"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e9967af365d4d5e74daf85111fe6f47.js"
+  "/precache-manifest.c10f995760201b878ceec93b8649e8a8.js"
 );
 
 self.addEventListener('message', (event) => {
