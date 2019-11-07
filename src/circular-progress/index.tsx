b55bd0d122dfd450f0b01c05ac4805a6935598cf
me@jamesmfriedman.com <me@jamesmfriedman.com>
2019-11-07 15:46:54 -0500

    refactor(CircularProgress): Refactor to be more idiomatic react

--- src/circular-progress/index.tsx
+++ src/circular-progress/index.tsx
@@ -1,6 +1,6 @@
 import * as RMWC from '@rmwc/types';
 import * as React from 'react';
-import { componentFactory } from '@rmwc/base';
+import { useClassNames, useTag } from '@rmwc/base/component';
 
 const SIZE_MAP: { [key: string]: number } = {
   xsmall: 18,
@@ -22,68 +22,71 @@ export interface CircularProgressProps {
   size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | number;
 }
 
-/** A Circular Progress indicator. */
-export const CircularProgress = componentFactory<CircularProgressProps>({
-  displayName: 'CircularProgress',
-  classNames: props => [
+export const CircularProgress = React.forwardRef<
+  any,
+  CircularProgressProps & Omit<RMWC.ComponentProps, 'size'>
+>(function CircularProgress(props, ref) {
+  const Tag = useTag(props, 'div');
+
+  const { size = 'medium', max = 1, min = 0, progress, ...rest } = props;
+
+  const className = useClassNames(props, [
     'rmwc-circular-progress',
     {
       [`rmwc-circular-progress--size-${props.size}`]:
         typeof props.size === 'string',
-      'rmwc-circular-progress--indeterminate': props.progress === undefined,
+      'rmwc-circular-progress--indeterminate': progress === undefined,
       'rmwc-circular-progress--thickerstroke':
-        !!props.size && (SIZE_MAP[props.size] || Number(props.size)) > 36
+        !!props.size && (SIZE_MAP[size] || Number(size)) > 36
     }
-  ],
-  useRender: (
-    { size = 'medium', max = 1, min = 0, progress, ...rest },
-    ref,
-    Tag
-  ) => {
-    const style = !SIZE_MAP[size]
-      ? { ...rest.style, fontSize: Number(size) }
-      : rest.style;
-    const _size = SIZE_MAP[size] || Number(size);
+  ]);
 
-    const calculateRatio = (value: number) => {
-      if (value < min) return 0;
-      if (value > max) return 1;
-      return (value - min) / (max - min);
-    };
+  const style = !SIZE_MAP[size]
+    ? { ...rest.style, fontSize: Number(size) }
+    : rest.style;
+  const _size = SIZE_MAP[size] || Number(size);
 
-    const circularStyle = (size: number) => {
-      return progress !== undefined
-        ? {
-            strokeDasharray: `${2 *
-              Math.PI *
-              (size / 2.4) *
-              calculateRatio(progress)}, 666.66%`
-          }
-        : undefined;
-    };
+  const calculateRatio = (value: number) => {
+    if (value < min) return 0;
+    if (value > max) return 1;
+    return (value - min) / (max - min);
+  };
 
-    return (
-      <Tag
-        aria-valuenow={progress}
-        aria-valuemin={min}
-        aria-valuemax={max}
-        ref={ref}
-        {...rest}
-        style={style}
+  const circularStyle = (size: number) => {
+    return progress !== undefined
+      ? {
+          strokeDasharray: `${2 *
+            Math.PI *
+            (size / 2.4) *
+            calculateRatio(progress)}, 666.66%`
+        }
+      : undefined;
+  };
+
+  return (
+    <Tag
+      aria-valuenow={progress}
+      aria-valuemin={min}
+      aria-valuemax={max}
+      ref={ref}
+      {...rest}
+      style={style}
+      className={className}
+    >
+      <svg
+        className="rmwc-circular-progress__circle"
+        viewBox={`0 0 ${_size} ${_size}`}
       >
-        <svg
-          className="rmwc-circular-progress__circle"
-          viewBox={`0 0 ${_size} ${_size}`}
-        >
-          <circle
-            className="rmwc-circular-progress__path"
-            style={circularStyle(_size)}
-            cx={_size / 2}
-            cy={_size / 2}
-            r={_size / 2.4}
-          />
-        </svg>
-      </Tag>
-    );
-  }
+        <circle
+          className="rmwc-circular-progress__path"
+          style={circularStyle(_size)}
+          cx={_size / 2}
+          cy={_size / 2}
+          r={_size / 2.4}
+        />
+      </svg>
+    </Tag>
+  );
 });
+
+CircularProgress.displayName = 'CircularProgress';
