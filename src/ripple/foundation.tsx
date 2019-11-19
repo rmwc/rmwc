45208b15f7ec0ae23fa94558d2de334e2ca9c95b
me@jamesmfriedman.com <me@jamesmfriedman.com>
2019-11-18 23:33:11 -0500

    refactor(Ripple): Update to new foundation pattern

---
+++ src/ripple/foundation.tsx
@@ -0,0 +1,194 @@
+import { RippleProps } from './';
+import { useFoundation } from '@rmwc/base';
+import { matches, applyPassive } from '@rmwc/base';
+import { EventType, SpecificEventListener } from '@material/base/types';
+
+import { MDCRippleFoundation, util } from '@material/ripple';
+import { useCallback, useRef, useEffect } from 'react';
+
+type ActivateEventTypes<S> =
+  | React.MouseEvent<S>
+  | React.TouchEvent<S>
+  | React.KeyboardEvent<S>
+  | React.FocusEvent<S>;
+
+export const useRippleFoundation = (
+  props: RippleProps & React.HTMLProps<any> & { domNode?: Element }
+) => {
+  const isTouched = useRef(false);
+
+  const { foundation, ...elements } = useFoundation({
+    props,
+    elements: {
+      rootEl: true,
+      surfaceEl: true
+    },
+    foundation: ({ rootEl, surfaceEl, getProps }) => {
+      return new MDCRippleFoundation({
+        browserSupportsCssVars: () => util.supportsCssVariables(window),
+        isUnbounded: () => !!getProps().unbounded,
+        isSurfaceActive: () => {
+          if (rootEl.ref) {
+            return matches(rootEl.ref, ':active');
+          }
+          return false;
+        },
+        isSurfaceDisabled: () => !!getProps().disabled,
+        addClass: (className: string) => surfaceEl.addClass(className),
+        removeClass: (className: string) => surfaceEl.removeClass(className),
+        containsEventTarget: (target: HTMLElement) =>
+          !!rootEl.ref && rootEl.ref.contains(target),
+        registerInteractionHandler: <K extends EventType>(
+          evtType: K,
+          handler: SpecificEventListener<K>
+        ): void => rootEl.addEventListener(evtType, handler),
+        deregisterInteractionHandler: <K extends EventType>(
+          evtType: K,
+          handler: SpecificEventListener<K>
+        ): void => rootEl.removeEventListener(evtType, handler),
+        registerDocumentInteractionHandler: <K extends EventType>(
+          evtType: K,
+          handler: SpecificEventListener<K>
+        ): void =>
+          document.documentElement.addEventListener(
+            evtType,
+            handler,
+            applyPassive()
+          ),
+        deregisterDocumentInteractionHandler: <K extends EventType>(
+          evtType: K,
+          handler: SpecificEventListener<K>
+        ) =>
+          document.documentElement.removeEventListener(
+            evtType,
+            handler,
+            applyPassive() as any
+          ),
+        registerResizeHandler: (
+          handler: SpecificEventListener<'resize'>
+        ): void => window.addEventListener('resize', handler),
+        deregisterResizeHandler: (
+          handler: SpecificEventListener<'resize'>
+        ): void => window.removeEventListener('resize', handler),
+        updateCssVariable: (varName: string, value: string) =>
+          surfaceEl.setStyle(varName, value),
+        computeBoundingRect: () =>
+          rootEl.ref
+            ? rootEl.ref.getBoundingClientRect()
+            : ({ width: 0, height: 0 } as ClientRect),
+        getWindowPageOffset: () => ({
+          x: window.pageXOffset,
+          y: window.pageYOffset
+        })
+      });
+    }
+  });
+
+  const { rootEl } = elements;
+
+  const activateRipple = useCallback(
+    (evt: ActivateEventTypes<HTMLElement> & Event) => {
+      // https://reactjs.org/docs/events.html#event-pooling
+      evt.persist();
+      foundation.activate(evt);
+    },
+    [foundation]
+  );
+
+  const deactivateRipple = useCallback(
+    (evt: ActivateEventTypes<HTMLElement>) => {
+      // https://reactjs.org/docs/events.html#event-pooling
+      evt.persist();
+      foundation.deactivate();
+    },
+    [foundation]
+  );
+
+  const handleFocus = useCallback(
+    (evt: React.FocusEvent<HTMLElement>) => {
+      props.onFocus?.(evt);
+      foundation.handleFocus();
+    },
+    [foundation, props.onFocus]
+  );
+
+  const handleBlur = useCallback(
+    (evt: React.FocusEvent<HTMLElement>) => {
+      props.onBlur?.(evt);
+      foundation.handleBlur();
+    },
+    [foundation, props.onBlur]
+  );
+
+  const handleMouseDown = useCallback(
+    (evt: React.MouseEvent<HTMLElement> & MouseEvent) => {
+      props.onMouseDown?.(evt);
+      if (!isTouched.current) {
+        activateRipple(evt);
+      }
+
+      isTouched.current = false;
+    },
+    [props.onMouseDown, activateRipple]
+  );
+
+  const handleMouseUp = useCallback(
+    (evt: React.MouseEvent<HTMLElement>) => {
+      props.onMouseUp?.(evt);
+      deactivateRipple(evt);
+    },
+    [props.onMouseUp, deactivateRipple]
+  );
+
+  const handleTouchStart = useCallback(
+    (evt: React.TouchEvent<HTMLElement> & TouchEvent) => {
+      isTouched.current = true;
+      props.onTouchStart?.(evt);
+      activateRipple(evt);
+    },
+    [props.onTouchStart, activateRipple]
+  );
+
+  const handleTouchEnd = useCallback(
+    (evt: React.TouchEvent<HTMLElement>) => {
+      props.onTouchEnd?.(evt);
+      deactivateRipple(evt);
+    },
+    [props.onTouchEnd, deactivateRipple]
+  );
+
+  const handleKeyDown = useCallback(
+    (evt: React.KeyboardEvent<HTMLElement> & KeyboardEvent) => {
+      props.onKeyDown?.(evt);
+      activateRipple(evt);
+    },
+    [props.onKeyDown, activateRipple]
+  );
+
+  const handleKeyUp = useCallback(
+    (evt: React.KeyboardEvent<HTMLElement>) => {
+      props.onKeyUp?.(evt);
+      deactivateRipple(evt);
+    },
+    [props.onKeyUp, deactivateRipple]
+  );
+
+  rootEl.setProp('onFocus', handleFocus, true);
+  rootEl.setProp('onBlur', handleBlur, true);
+  rootEl.setProp('onMouseDown', handleMouseDown, true);
+  rootEl.setProp('onMouseUp', handleMouseUp, true);
+  rootEl.setProp('onTouchStart', handleTouchStart, true);
+  rootEl.setProp('onTouchEnd', handleTouchEnd, true);
+  rootEl.setProp('onKeyDown', handleKeyDown, true);
+  rootEl.setProp('onKeyUp', handleKeyUp, true);
+
+  useEffect(() => {
+    rootEl.setRef(props.domNode);
+  }, [rootEl, props.domNode]);
+
+  useEffect(() => {
+    foundation.setUnbounded(!!props.unbounded);
+  }, [props.unbounded, foundation]);
+
+  return { ...elements };
+};
--- src/ripple/index.tsx
+++ src/ripple/index.tsx
@@ -1,15 +1,9 @@
 import * as RMWC from '@rmwc/types';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { MDCRippleFoundation, util } from '@material/ripple';
-import {
-  classNames,
-  FoundationComponent,
-  matches,
-  applyPassive
-} from '@rmwc/base';
+import { classNames } from '@rmwc/base';
 import { useProviderContext } from '@rmwc/provider';
-import { EventType, SpecificEventListener } from '@material/base/types';
+import { useRippleFoundation } from './foundation';
 
 export interface RippleSurfaceProps {
   className: string;
@@ -30,245 +24,113 @@ export interface RippleProps {
   surface?: boolean;
 }
 
-type ActivateEventTypes<S> =
-  | React.MouseEvent<S>
-  | React.TouchEvent<S>
-  | React.KeyboardEvent<S>
-  | React.FocusEvent<S>;
-
 const RippleSurfaceContext = React.createContext({});
 
 /** A component for adding Ripples to other components. */
-export class Ripple extends FoundationComponent<
-  MDCRippleFoundation,
-  RippleProps
-> {
-  static shouldDebounce = true;
-  static displayName = 'Ripple';
+const withDomNode = () => <P extends any>(
+  Component: React.ComponentType<P>
+): React.ComponentType<P & { domNode?: Element }> => {
+  return class extends React.Component<
+    { children: React.ReactNode } & P & { domNode?: Element }
+  > {
+    state = { domNode: null };
 
-  private root = this.createElement('root');
-  private surface = this.createElement('surface');
-  private isTouched = false;
-
-  constructor(props: RippleProps) {
-    super(props);
-
-    this.handleFocus = this.handleFocus.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-    this.handleMouseDown = this.handleMouseDown.bind(this);
-    this.handleMouseUp = this.handleMouseUp.bind(this);
-    this.handleTouchStart = this.handleTouchStart.bind(this);
-    this.handleTouchEnd = this.handleTouchEnd.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-  }
-
-  getDefaultFoundation() {
-    return new MDCRippleFoundation({
-      browserSupportsCssVars: () => util.supportsCssVariables(window),
-      isUnbounded: () => !!this.props.unbounded,
-      isSurfaceActive: () => {
-        if (this.root.ref) {
-          return matches(this.root.ref, ':active');
-        }
-        return false;
-      },
-      isSurfaceDisabled: () => !!this.props.disabled,
-      addClass: (className: string) => this.surface.addClass(className),
-      removeClass: (className: string) => this.surface.removeClass(className),
-      containsEventTarget: (target: HTMLElement) =>
-        !!this.root.ref && this.root.ref.contains(target),
-      registerInteractionHandler: <K extends EventType>(
-        evtType: K,
-        handler: SpecificEventListener<K>
-      ): void => this.root.addEventListener(evtType, handler),
-      deregisterInteractionHandler: <K extends EventType>(
-        evtType: K,
-        handler: SpecificEventListener<K>
-      ): void => this.root.removeEventListener(evtType, handler),
-      registerDocumentInteractionHandler: <K extends EventType>(
-        evtType: K,
-        handler: SpecificEventListener<K>
-      ): void =>
-        document.documentElement.addEventListener(
-          evtType,
-          handler,
-          applyPassive()
-        ),
-      deregisterDocumentInteractionHandler: <K extends EventType>(
-        evtType: K,
-        handler: SpecificEventListener<K>
-      ) =>
-        document.documentElement.removeEventListener(
-          evtType,
-          handler,
-          applyPassive() as any
-        ),
-      registerResizeHandler: (handler: SpecificEventListener<'resize'>): void =>
-        window.addEventListener('resize', handler),
-      deregisterResizeHandler: (
-        handler: SpecificEventListener<'resize'>
-      ): void => window.removeEventListener('resize', handler),
-      updateCssVariable: (varName: string, value: string) =>
-        this.surface.setStyle(varName, value),
-      computeBoundingRect: () =>
-        this.root.ref
-          ? this.root.ref.getBoundingClientRect()
-          : ({ width: 0, height: 0 } as ClientRect),
-      getWindowPageOffset: () => ({
-        x: window.pageXOffset,
-        y: window.pageYOffset
-      })
-    });
-  }
-
-  sync(props: RippleProps, prevProps: RippleProps) {
-    this.root.setRef(ReactDOM.findDOMNode(this));
-
-    if (props.unbounded !== prevProps.unbounded) {
-      this.foundation.setUnbounded(!!props.unbounded);
-    }
-  }
-
-  handleFocus(evt: React.FocusEvent<HTMLElement>) {
-    this.props.onFocus && this.props.onFocus(evt);
-    this.foundation && this.foundation.handleFocus();
-  }
-
-  handleBlur(evt: React.FocusEvent<HTMLElement>) {
-    this.props.onBlur && this.props.onBlur(evt);
-    this.foundation.handleBlur();
-  }
-
-  handleMouseDown(evt: React.MouseEvent<HTMLElement> & MouseEvent) {
-    this.props.onMouseDown && this.props.onMouseDown(evt);
-    if (!this.isTouched) {
-      this.activateRipple(evt);
+    componentDidMount() {
+      this.setState({ domNode: ReactDOM.findDOMNode(this) as Element });
     }
 
-    this.isTouched = false;
-  }
+    componentDidUpdate() {
+      const rootRippleElement = ReactDOM.findDOMNode(this) as Element;
 
-  handleMouseUp(evt: React.MouseEvent<HTMLElement>) {
-    this.props.onMouseUp && this.props.onMouseUp(evt);
-    this.deactivateRipple(evt);
-  }
-
-  handleTouchStart(evt: React.TouchEvent<HTMLElement> & TouchEvent) {
-    this.isTouched = true;
-    this.props.onTouchStart && this.props.onTouchStart(evt);
-    this.activateRipple(evt);
-  }
-
-  handleTouchEnd(evt: React.TouchEvent<HTMLElement>) {
-    this.props.onTouchEnd && this.props.onTouchEnd(evt);
-    this.deactivateRipple(evt);
-  }
-
-  handleKeyDown(evt: React.KeyboardEvent<HTMLElement> & KeyboardEvent) {
-    this.props.onKeyDown && this.props.onKeyDown(evt);
-    this.activateRipple(evt);
-  }
-
-  handleKeyUp(evt: React.KeyboardEvent<HTMLElement>) {
-    this.props.onKeyUp && this.props.onKeyUp(evt);
-    this.deactivateRipple(evt);
-  }
-
-  activateRipple(evt: ActivateEventTypes<HTMLElement> & Event) {
-    // https://reactjs.org/docs/events.html#event-pooling
-    evt.persist();
-    this.foundation.activate(evt);
-  }
-
-  deactivateRipple(evt: ActivateEventTypes<HTMLElement>) {
-    // https://reactjs.org/docs/events.html#event-pooling
-    evt.persist();
-    this.foundation.deactivate();
-  }
-
-  render() {
-    const {
-      children,
-      className,
-      primary,
-      accent,
-      unbounded,
-      surface,
-      ...rest
-    } = this.props;
-
-    const child = React.Children.only(children);
-
-    if (!React.isValidElement<React.HTMLProps<any>>(child)) {
-      return null;
-    }
-
-    // This flag really determines a lot
-    // is surfaceIsRoot is true, then the surface props are spread
-    // to the underlying component, otherwise the only place they
-    // can be picked up is by the context consumer
-    const surfaceIsRoot = !surface || !unbounded;
-
-    const unboundedProp = unbounded
-      ? { 'data-mdc-ripple-is-unbounded': true }
-      : {};
-
-    const rippleSurfaceProps = surfaceIsRoot
-      ? this.surface.props({ style: child.props.style })
-      : {};
-
-    let finalClassNames = classNames(
-      className !== child.props.className && className,
-      rippleSurfaceProps.className,
-      child.props.className,
-      {
-        'mdc-ripple-surface':
-          typeof surface === 'boolean' ? surface : surface === undefined,
-        'mdc-ripple-surface--primary': primary,
-        'mdc-ripple-surface--accent': accent
+      if (rootRippleElement !== this.state.domNode) {
+        this.setState({ rootRippleElement });
       }
-    );
-
-    // Fixes a ripple artifact issue
-    // that is caused when clicking a button disables it
-    // https://codesandbox.io/s/842vo56019
-    if (rest.disabled) {
-      finalClassNames = finalClassNames.replace(
-        'mdc-ripple-upgraded--background-focused',
-        ''
-      );
     }
 
-    // do some crazy props merging...
-    const content = React.cloneElement(child, {
-      ...child.props,
-      ...unboundedProp,
-      ...this.root.props({
-        ...rest,
-        style: child.props.style,
-        ...rippleSurfaceProps,
-        className: finalClassNames
-      }),
-      onFocus: this.handleFocus,
-      onBlur: this.handleBlur,
-      onMouseDown: this.handleMouseDown,
-      onMouseUp: this.handleMouseUp,
-      onTouchStart: this.handleTouchStart,
-      onTouchEnd: this.handleTouchEnd,
-      onKeyDown: this.handleKeyDown,
-      onKeyUp: this.handleKeyUp
-    });
+    render() {
+      return <Component {...this.props} domNode={this.state.domNode} />;
+    }
+  };
+};
 
-    return (
-      <RippleSurfaceContext.Provider
-        value={this.surface.props({ style: child.props.style })}
-      >
-        {content}
-      </RippleSurfaceContext.Provider>
+export const Ripple = withDomNode()(function Ripple(
+  props: RippleProps & RMWC.ComponentProps & { domNode?: Element }
+) {
+  const {
+    children,
+    className,
+    primary,
+    accent,
+    unbounded,
+    surface,
+    domNode,
+    ...rest
+  } = props;
+
+  const { rootEl, surfaceEl } = useRippleFoundation(props);
+
+  const child = React.Children.only(children);
+
+  if (!React.isValidElement<React.HTMLProps<any>>(child)) {
+    return null;
+  }
+
+  // This flag really determines a lot
+  // is surfaceIsRoot is true, then the surface props are spread
+  // to the underlying component, otherwise the only place they
+  // can be picked up is by the context consumer
+  const surfaceIsRoot = !surface || !unbounded;
+
+  const unboundedProp = unbounded
+    ? { 'data-mdc-ripple-is-unbounded': true }
+    : {};
+
+  const rippleSurfaceProps = surfaceIsRoot
+    ? surfaceEl.props({ style: child.props.style })
+    : {};
+
+  let finalClassNames = classNames(
+    className !== child.props.className && className,
+    rippleSurfaceProps.className,
+    child.props.className,
+    {
+      'mdc-ripple-surface':
+        typeof surface === 'boolean' ? surface : surface === undefined,
+      'mdc-ripple-surface--primary': primary,
+      'mdc-ripple-surface--accent': accent
+    }
+  );
+
+  // Fixes a ripple artifact issue
+  // that is caused when clicking a button disables it
+  // https://codesandbox.io/s/842vo56019
+  if (rest.disabled) {
+    finalClassNames = finalClassNames.replace(
+      'mdc-ripple-upgraded--background-focused',
+      ''
     );
   }
-}
+
+  // do some crazy props merging...
+  const content = React.cloneElement(child, {
+    ...child.props,
+    ...unboundedProp,
+    ...rootEl.props({
+      ...rest,
+      style: child.props.style,
+      ...rippleSurfaceProps,
+      className: finalClassNames
+    })
+  });
+
+  return (
+    <RippleSurfaceContext.Provider
+      value={surfaceEl.props({ style: child.props.style })}
+    >
+      {content}
+    </RippleSurfaceContext.Provider>
+  );
+});
 
 export const RippleSurface = ({
   className,
