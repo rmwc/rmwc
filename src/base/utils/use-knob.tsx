2a098d3545f89e18aacdfbeeb775a780e85a7097
me@jamesmfriedman.com <me@jamesmfriedman.com>
2020-03-27 15:05:51 -0400

    Fixing issue with storybook knobs syncing

--- src/base/utils/use-knob.tsx
+++ src/base/utils/use-knob.tsx
@@ -2,15 +2,22 @@
 import { useState } from 'react';
 import * as knobTypes from '@storybook/addon-knobs';
 
+import { manager } from '@storybook/addon-knobs/dist/registerKnobs';
+
 export const useKnob = (
   knobType: keyof typeof knobTypes,
   name: string,
   defaultValue: any
 ) => {
   const knobFunc = knobTypes[knobType] as any;
-  const [stateValue, stateSetter] = useState(defaultValue);
-
+  const [stateValue, _stateSetter] = useState(defaultValue);
   const knobValue = knobFunc(name, stateValue);
+
+  const stateSetter = (value: any) => {
+    _stateSetter(value);
+    manager.knob(name, stateValue);
+  };
+
   if (knobValue !== stateValue) {
     stateSetter(knobValue);
   }
