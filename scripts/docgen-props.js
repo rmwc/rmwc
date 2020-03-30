93ef942c3b8290db77bfaaa14da061ccfe7e8d77
me@jamesmfriedman.com <me@jamesmfriedman.com>
2020-03-29 22:54:17 -0400

    Updating docs

--- scripts/docgen-props.js
+++ scripts/docgen-props.js
@@ -3,27 +3,27 @@ const getChangedPackages = require('./get-changed-packages');
 const getAllPackages = require('./get-all-packages');
 
 const getPackages =
-  //getAllPackages
+  //getAllPackages;
   getChangedPackages;
 
 try {
   getPackages()
-    .filter(name => !['base', 'rmwc', '@types'].includes(name))
-    .forEach(d => {
+    .filter((name) => !['base', 'rmwc', '@types'].includes(name))
+    .forEach((d) => {
       console.log(`Building Docs For: ${d}`);
       const proc = exec(
-        `documentalist "./src/${d}/!(node_modules)/!(spec|story).tsx" --no-css --no-md > ./src/${d}/generated-props.json`
+        `documentalist "./src/${d}/**/!(spec|story).tsx" --no-css --no-md > ./src/${d}/generated-props.json`
       );
 
-      proc.stdout.on('data', data => {
+      proc.stdout.on('data', (data) => {
         console.log(data.toString());
       });
 
-      proc.stderr.on('data', data => {
+      proc.stderr.on('data', (data) => {
         console.log('Error: ' + data.toString());
       });
 
-      proc.on('exit', code => {});
+      proc.on('exit', (code) => {});
     });
 } catch (err) {
   console.error(err.toString());
