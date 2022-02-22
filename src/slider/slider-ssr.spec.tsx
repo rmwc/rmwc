e1e912f54ac50831b148e9c29b61abfc17b6a03e
Emilie <12230809+EmiBemi@users.noreply.github.com>
2022-02-22 12:57:36 +0100

    fix(select): change ssr test to run on node

--- src/slider/slider-ssr.spec.tsx
+++ src/slider/slider-ssr.spec.tsx
@@ -1,18 +1,9 @@
 /**
- * @jest-environment jsdom
+ * @jest-environment node
  */
 import React from 'react';
 import { renderToString as mount } from 'react-dom/server';
 import { Slider } from './';
-
-/*
-As part of bumping MWC to v6, these tests are failing due to window not being defined.
-MWC are fully aware of this server-side rendering error and already have a fix for it in v7.
-See https://github.com/material-components/material-components-web/commit/95c73550e886c2832aa42cd065552551b6690a61.
-So when we bump MWC from v6 to v7, we can reintroduce these tests.
-What to do when bumped to v7:
-- Change jest-environemnt to 'node' on line 2: @jest-environemnt node
-*/
 
 describe('Slider SSR', () => {
   it('renders uncontrolled', () => {
