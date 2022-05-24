7cd10d57fd9d8228138922157de3a1b160c0866d
James Friedman <me@jamesmfriedman.com>
2022-05-24 15:45:37 -0400

    Updating cov

--- scripts/test.js
+++ scripts/test.js
@@ -1,16 +1,16 @@
 const fs = require('fs-extra');
 const childProcess = require('child_process');
 
-fs.readdir('react-versions', function(err, files) {
-  const reactVersions = files.filter(f => f.startsWith('react-'));
+fs.readdir('react-versions', function (err, files) {
+  const reactVersions = files.filter((f) => f.startsWith('react-'));
 
   reactVersions.forEach((reactVersion, index, arr) => {
     const versionNum = reactVersion.split('-')[1];
-    const isLastRun = index === arr.length - 1;
+    const isFirstRun = index === 0;
     console.log('Running tests for', versionNum);
     childProcess.execSync(
       //prettier-ignore
-      `export REACT_TEST_VERSION=${versionNum} CI=true && react-app-rewired test --maxWorkers=2 --env=jsdom ${isLastRun ? '--coverage' : ''}`,
+      `export REACT_TEST_VERSION=${versionNum} CI=true && react-app-rewired test --maxWorkers=2 --env=jsdom ${isFirstRun ? '--coverage' : ''}`,
       { stdio: [0, 1, 2] }
     );
   });
