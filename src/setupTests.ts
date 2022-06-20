c0e9ccf9c98476f60663e870bd274775fd9b20a3
Emilie <12230809+EmiBemi@users.noreply.github.com>
2022-06-20 13:16:29 +0200

    remove react 16 support for tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,14 +1,11 @@
 import React from 'react';
-import Adapter16 from 'enzyme-adapter-react-16';
 import Adapter17 from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme from 'enzyme';
 // @ts-ignore
 import rmwcTestPolyfill from './base/test-polyfill';
 import '@testing-library/jest-dom/extend-expect';
 
-const Adapter = React.version.startsWith('17') ? Adapter17 : Adapter16;
-
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter17() });
 rmwcTestPolyfill();
 
 const consoleError = console.error;
