f0743643dfa3610876a7c04cce1a7d275dcb346c
Emilie <12230809+EmiBemi@users.noreply.github.com>
2022-06-22 10:08:55 +0200

    rewrite top-app-bar test to RTL

--- src/tooltip/tooltip.spec.tsx
+++ src/tooltip/tooltip.spec.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Tooltip } from './';
 import { RMWCProvider } from '../provider';
-import { wait } from '@rmwc/base/utils/test-utils';
 
 describe('Tooltip', () => {
   it('renders', async () => {
