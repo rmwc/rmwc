1de364cd57f7a4c692d7b9d6c95cd5a27bf5f9cd
Emilie <12230809+EmiBemi@users.noreply.github.com>
2023-04-24 12:18:11 +0200

    fix(chips): Handle interaction

--- src/chip/action/foundation.tsx
+++ src/chip/action/foundation.tsx
@@ -2,15 +2,25 @@ import { useFoundation } from '@rmwc/base';
 import {
   MDCChipActionAdapter,
   MDCChipActionAttributes,
-  MDCChipActionCssClasses,
+  MDCChipActionFocusBehavior,
   MDCChipPrimaryActionFoundation,
   MDCChipTrailingActionFoundation
 } from '@material/chips';
-import { ActionHTMLProps, PrimaryActionProps } from '.';
+import {
+  ActionApi,
+  ActionHTMLProps,
+  PrimaryActionProps,
+  TrailingActionApi
+} from '.';
+import { useActionContext } from '../action-context';
+import { useEffect, useMemo } from 'react';
 
 export const usePrimaryActionFoundation = (
-  props: PrimaryActionProps & ActionHTMLProps
+  props: (PrimaryActionProps | TrailingActionApi) & ActionHTMLProps,
+  actionType: 'trailing' | 'primary'
 ) => {
+  const actionContextApi = useActionContext();
+
   const foundationWithElements = useFoundation({
     props,
     elements: { rootEl: true },
@@ -28,9 +38,7 @@ export const usePrimaryActionFoundation = (
         removeAttribute: (attr: MDCChipActionAttributes) =>
           rootEl.ref?.removeAttribute(attr)
       };
-      if (
-        rootEl.ref?.classList.contains(MDCChipActionCssClasses.TRAILING_ACTION)
-      ) {
+      if (actionType === 'trailing') {
         return new MDCChipTrailingActionFoundation(adapter);
       }
       return new MDCChipPrimaryActionFoundation(adapter);
@@ -39,14 +47,35 @@ export const usePrimaryActionFoundation = (
 
   const { foundation, rootEl } = foundationWithElements;
 
+  const actionApi = useMemo<ActionApi>(() => {
+    return {
+      actionType: foundation.actionType(),
+      isSelectAble: () => foundation.isSelectable(),
+      isSelected: () => foundation.isSelected(),
+      isFocusable: () => foundation.isFocusable(),
+      isDisabled: () => foundation.isDisabled(),
+      setDisabled: (isDisabled: boolean) => foundation.setDisabled(isDisabled),
+      setFocus: (behavior: MDCChipActionFocusBehavior) =>
+        foundation.setFocus(behavior),
+      setSelected: (isSelected: boolean) => foundation.setSelected(isSelected)
+    };
+  }, [foundation]);
+
+  useEffect(() => {
+    // set action for primary and trailing
+    actionContextApi.registerAction(actionApi);
+
+    return () => {
+      actionContextApi.unregisterAction(actionApi);
+    };
+  }, [actionContextApi, actionApi]);
+
   const handleClick = (event: any) => {
     props.onClick?.(event);
-    props.onInteraction?.(event);
     foundation.handleClick();
   };
 
   const handleKeyDown = (event: KeyboardEvent) => {
-    props.onInteraction?.(event as any);
     foundation.handleKeydown(event);
   };
 
