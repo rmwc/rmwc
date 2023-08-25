eefcac36407d034f846546752c5db123a2568481
Emilie <12230809+EmiBemi@users.noreply.github.com>
2023-08-25 15:07:38 +0200

    fix(select): renderToPortal on enhanced menu

--- src/select/select/index.tsx
+++ src/select/select/index.tsx
@@ -12,7 +12,8 @@ import {
   MenuItems,
   MenuProps,
   MenuApi,
-  MenuOnSelectEventT
+  MenuOnSelectEventT,
+  MenuSurfaceAnchor
 } from '@rmwc/menu';
 import { ListGroup, ListGroupSubheader, ListDivider } from '@rmwc/list';
 import { withRipple } from '@rmwc/ripple';
@@ -27,6 +28,10 @@ export interface FormattedOption
   value?: string;
   options?: FormattedOption[];
 }
+
+export type EnhancedType =
+  | boolean
+  | (MenuProps & { ref?: React.Ref<HTMLElement> });
 
 /** A Select Component */
 export interface SelectProps {
@@ -49,7 +54,7 @@ export interface SelectProps {
   /** Makes the Select required.  */
   required?: boolean;
   /** Renders a non native / enhanced dropdown */
-  enhanced?: boolean | (MenuProps & { ref?: React.Ref<HTMLElement> });
+  enhanced?: EnhancedType;
   /** Props for the root element. By default, additional props spread to the native select element.  */
   rootProps?: Object;
   /** A reference to the native select element. Not applicable when `enhanced` is true. */
@@ -273,7 +278,10 @@ const EnhancedMenu = React.forwardRef(
                   )}
                   <MenuItems>
                     {options.map(({ label, ...option }) =>
-                      renderOption({ label, option: option as FormattedOption })
+                      renderOption({
+                        label,
+                        option: option as FormattedOption
+                      })
                     )}
                   </MenuItems>
                   {i < selectOptions.length - 1 && <ListDivider />}
@@ -289,6 +297,19 @@ const EnhancedMenu = React.forwardRef(
     );
   }
 );
+
+const EnhancedMenuWrapper = ({
+  enhanced,
+  children
+}: {
+  enhanced?: EnhancedType;
+  children?: React.ReactNode;
+}) =>
+  enhanced ? (
+    <MenuSurfaceAnchor>{children}</MenuSurfaceAnchor>
+  ) : (
+    <>{children}</>
+  );
 
 export const Select: RMWC.ComponentType<
   SelectProps,
@@ -384,7 +405,7 @@ export const Select: RMWC.ComponentType<
   };
 
   return (
-    <>
+    <EnhancedMenuWrapper enhanced={enhanced}>
       <Tag {...rootProps} element={rootEl} ref={ref} className={className}>
         <AnchorEl
           className="mdc-select__anchor"
@@ -461,7 +482,7 @@ export const Select: RMWC.ComponentType<
         )}
       </Tag>
       {renderHelpText()}
-    </>
+    </EnhancedMenuWrapper>
   );
 });
 
