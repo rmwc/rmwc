a239b455789c8d7ded5b284546dbb32ddacb2cb0
Jens Borch Christiansen <jens.borch@gmail.com>
2023-10-20 20:51:13 +0200

    chore: updated documentalist

--- scripts/docgen-props.js
+++ scripts/docgen-props.js
@@ -18,7 +18,7 @@ try {
     });
 
     proc.stderr.on('data', (data) => {
-      console.log('Error: ' + data.toString());
+      console.log('Error: Command "' + proc.spawnargs + '" failed with:  ' + data.toString());
     });
 
     proc.on('exit', (code) => {});
