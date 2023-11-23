e9b51d38c143f15461eb256135e1b8da71a3c70f
Jens Borch Christiansen <jenc@nykredit.dk>
2023-11-23 14:04:14 +0100

    fix(docgen-markdown): updated script to work with Nx

---
+++ scripts/docgen-markdown.mjs
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import * as fs from 'fs';
+import getPackages from './get-packages.js';
+import { default as TurndownService } from 'turndown';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const root = path.resolve(__dirname, '../');
+
+const turndownService = TurndownService({ codeBlockStyle: 'fenced' });
+
+turndownService.addRule('code', {
+  filter: ['pre'],
+  replacement: function (content) {
+    return '```js' + content + '```';
+  }
+});
+
+const distPath = path.resolve(
+  'dist',
+  'readmes',
+  'utils',
+  'readme',
+  'src',
+  'readmes'
+);
+
+const getMarkdown = async (packageName) => {
+  const readmeFiles = fs
+    .readdirSync(distPath)
+    .filter((fName) => fName.startsWith(packageName) && fName.endsWith('.mjs'));
+
+  const promises = readmeFiles.map(async (fName) => {
+    const docPath = path.resolve(distPath, fName);
+    const markdownOutputName = packageName.toUpperCase() + '.md';
+    const htmlOutputName = packageName + '.html';
+    const { default: Component } = await import(docPath);
+    const htmlContent = renderToStaticMarkup(React.createElement(Component));
+    const markdown = turndownService.turndown(htmlContent);
+    return new Promise((resolve) => {
+      fs.writeFile(path.resolve(distPath, htmlOutputName), htmlContent, () => {
+        resolve();
+      });
+      fs.writeFile(path.resolve(distPath, markdownOutputName), markdown, () => {
+        resolve();
+      });
+    });
+  });
+
+  return Promise.all(promises);
+};
+
+try {
+  const promises = getPackages(['readme']).map((d) => {
+    console.log(`Generating Markdown For: ${d}`);
+    return getMarkdown(d);
+  });
+
+  Promise.all(promises);
+} catch (err) {
+  console.error(err.toString());
+}
--- utils/readme/vite.config.ts
+++ utils/readme/vite.config.ts
@@ -4,9 +4,28 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/readme',
+  cacheDir: '../../node_modules/.vite/readmes',
 
   plugins: [react(), nxViteTsPaths()],
+
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: ['src/index.ts'],
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es']
+    },
+    rollupOptions: {
+      output: {
+        preserveModules: true,
+        dir: 'dist/readmes'
+      },
+      external: ['react', 'react-dom', 'react/jsx-runtime']
+    }
+  },
 
   test: {
     globals: true,
