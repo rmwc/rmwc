003a7b725e62b0b6b74a51c5b838120093b93170
Emilie <emilieortoft@gmail.com>
2023-11-28 15:24:52 +0100

    chore: write tests for theme utils

--- packages/theme/src/lib/utils.tsx
+++ packages/theme/src/lib/utils.tsx
@@ -142,13 +142,13 @@ const colorMap: { [key: string]: string } = {
   yellowgreen: '#9acd32'
 };
 
-const nameToHex = (name: string) => colorMap[name] || name;
+export const nameToHex = (name: string) => colorMap[name] || name;
 
-const hexToRgb = (hex: string) => {
+export const hexToRgb = (hex: string) => {
   if (hex.length === 4) {
     hex = hex + hex.slice(1);
   }
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result
     ? {
         r: parseInt(result[1], 16),
@@ -162,15 +162,15 @@ const hexToRgb = (hex: string) => {
       };
 };
 
-const luminance = (r: number, g: number, b: number) => {
-  var a = [r, g, b].map(function(v) {
+export const luminance = (r: number, g: number, b: number) => {
+  let a = [r, g, b].map(function (v) {
     v /= 255;
     return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
   });
   return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 };
 
-const contrast = (
+export const contrast = (
   rgb1: [number, number, number],
   rgb2: [number, number, number]
 ) => {
@@ -224,7 +224,7 @@ export const getAutoColorsForTheme = (colors: { [key: string]: string }) => {
           ? lightTextPalette
           : darkTextPalette;
 
-        paletteMap[key].forEach(k => {
+        paletteMap[key].forEach((k) => {
           acc[k[0]] = palette[k[1]];
         });
       }
